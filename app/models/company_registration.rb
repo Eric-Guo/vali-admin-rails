0a67be5be895b5608a2bac648747a968640053dd
Eric-Guo <eric@managebac.com>
2018-07-01 21:05:41 +0800

    Allow user select VerticalMarket instead of keyin.

--- app/models/company_registration.rb
+++ app/models/company_registration.rb
@@ -1,7 +1,7 @@
 class CompanyRegistration
   include ActiveModel::Model
   DEVISE_USER_EXT_FIELDS = %i[name title phone].freeze
-  COMPANY_REG_FIELDS = %i[co_name city district address vm_name].freeze
+  COMPANY_REG_FIELDS = %i[co_name city district address vm_id].freeze
   FORM_FIELDS = (DEVISE_USER_EXT_FIELDS + COMPANY_REG_FIELDS).freeze
   attr_accessor(*COMPANY_REG_FIELDS)
   validates_presence_of(*COMPANY_REG_FIELDS)
@@ -15,8 +15,7 @@ class CompanyRegistration
       co.address = address
     end
     CompanyUser.find_or_create_by!(company: company, user: @user)
-    vertical_market = VerticalMarket.find_by!(name: vm_name)
-    VerticalMarketCompany.find_or_create_by!(company: company, vertical_market: vertical_market)
+    VerticalMarketCompany.find_or_create_by!(company: company, vertical_market_id: vm_id)
     true
   end
 end
