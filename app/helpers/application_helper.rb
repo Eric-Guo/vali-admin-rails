03b7ebede0991262a33e7be1cda8979db206b87d
Eric-Guo <eric@managebac.com>
2018-07-03 20:22:52 +0800

    Display system role also after login.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
                       resource: resource.class.model_name.human.downcase)
     "#{sentence}#{messages}"
   end
+
+  def display_system_role(user)
+    return unless user.present?
+    vm = VerticalMarket.find_by admin: user
+    if vm.present?
+      content_tag :p, "#{vm.name}管理员", class: 'app-sidebar__user-designation'
+    end
+  end
 end
