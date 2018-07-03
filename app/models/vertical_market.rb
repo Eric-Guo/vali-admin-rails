345a7a5962a96ddf1d89a2da5cd74d22c240583e
Eric-Guo <eric@managebac.com>
2018-07-03 21:13:24 +0800

    Display users in vendor list

--- app/models/vertical_market.rb
+++ app/models/vertical_market.rb
@@ -1,5 +1,6 @@
 class VerticalMarket < ApplicationRecord
   has_many :vertical_market_companies
   has_many :companies, through: :vertical_market_companies
+  has_many :users, through: :companies
   belongs_to :admin, class_name: :User, foreign_key: :admin_id
 end
