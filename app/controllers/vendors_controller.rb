4d30e54af91f1a7a82bf9acb5081d797df2010f1
Eric-Guo <eric@managebac.com>
2018-07-03 21:13:24 +0800

    Display users in vendor list

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -4,10 +4,13 @@ class VendorsController < ApplicationController
     admined_vm = VerticalMarket.find_by admin: current_user
 
     @vendor_list_description = if current_user.super_admin?
+      @users = User.all
       '下表列出了所有代理商'
     elsif admined_vm.present?
-      "下表列出了#{vm.name}代理商"
+      @users = admined_vm.users
+      "下表列出了#{admined_vm.name}代理商"
     else
+      @users = []
       '请联系管理员获得访问此列表权限'
     end
   end
