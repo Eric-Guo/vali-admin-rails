0cab89e775582e332bcd7a9bf54708be8dfcdfa6
Eric-Guo <eric@managebac.com>
2018-07-11 23:18:06 +0800

    Simplify the logic in vendor controller

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -3,41 +3,29 @@ class VendorsController < ApplicationController
 
   def index
     @vendor_list_description = if current_user.super_admin?
-      users = User.all
       '下表列出了所有代理商'
-    elsif admined_vm.present?
-      users = admined_vm.users
-      "下表列出了#{admined_vm.name}代理商"
+    elsif current_user.admined_vm.present?
+      "下表列出了#{current_user.admined_vm.name}代理商"
     else
-      users = User.none
       I18n.t('ui.no_access_list')
     end
-    @users = users.where(locked_at: nil)
+    @users = current_user.managed_users.where(locked_at: nil)
   end
 
   def pending
     @vendor_list_description = if current_user.super_admin?
-      users = User.all
       '下表列出了所有锁定的代理商'
-    elsif admined_vm.present?
-      users = admined_vm.users
-      "下表列出了#{admined_vm.name}被锁定的代理商"
+    elsif current_user.admined_vm.present?
+      "下表列出了#{current_user.admined_vm.name}被锁定的代理商"
     else
-      users = User.none
       I18n.t('ui.no_access_list')
     end
-    @users = users.where.not(locked_at: nil)
+    @users = current_user.managed_users.where.not(locked_at: nil)
   end
 
   def unlock_access
-    @user = User.find params[:id]
+    @user = current_user.managed_users.find params[:id]
     @user.unlock_access!
     redirect_to vendors_path, status: :found, notice: "User #{@user.name} unlocked."
-  end
-
-  private
-
-  def admined_vm
-    @admined_vm ||= VerticalMarket.find_by admin: current_user
   end
 end
