7ad37efee56c84c84f71101a5a6ecb552e0fa5ba
Eric-Guo <eric@managebac.com>
2018-07-25 21:13:48 +0800

    Add company 2nd level create support.

---
+++ test/controllers/companies_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class CompaniesControllerTest < ActionDispatch::IntegrationTest
+  test 'should show index if sign in' do
+    sign_in users(:eric)
+    post create_user_companies_url, params: { user:
+      { email: '2nd@company.com', name: '金无命', title: '剑客',
+        phone: '13311825138', co_name: '金钱帮', city: '上海',
+        district: '杨浦', address: '山庄', vm_id: vertical_markets(:akg).id } }
+    user = User.find_by email: '2nd@company.com'
+    assert_equal user.name, '金无命'
+    assert_equal user.companies.first.name, '金钱帮'
+    assert_response :success
+  end
+end
