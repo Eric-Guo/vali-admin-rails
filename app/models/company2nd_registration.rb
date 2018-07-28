2bbfbed4bbcf3a93a9c49c5ef69cbc5fe5e1d11e
Eric-Guo <eric@managebac.com>
2018-07-28 19:35:55 +0800

    Record first level company.

--- app/models/company2nd_registration.rb
+++ app/models/company2nd_registration.rb
@@ -1,6 +1,6 @@
 class Company2ndRegistration
   include ActiveModel::Model
-  USER_FIELDS = %i[email name title phone].freeze
+  USER_FIELDS = %i[email name title phone create_user_via_company_id].freeze
   COMPANY_REG_FIELDS = %i[co_name city district address vm_id].freeze
   FORM_FIELDS = (USER_FIELDS + COMPANY_REG_FIELDS).freeze
   attr_accessor(*FORM_FIELDS)
@@ -17,6 +17,7 @@ class Company2ndRegistration
     company = Company.find_or_create_by(name: co_name, city: city) do |co|
       co.district = district
       co.address = address
+      co.managed_by_company_id = create_user_via_company_id
     end
     CompanyUser.find_or_create_by!(company: company, user: @user)
     VerticalMarketCompany.find_or_create_by!(company: company, vertical_market_id: vm_id)
