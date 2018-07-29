c6fddf291baa5169100c96fca73a07573d8b00c6
Eric-Guo <eric@managebac.com>
2018-07-29 17:02:24 +0800

    Hide course management if not applicant.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,9 @@
 class CoursesController < ApplicationController
   before_action :authenticate_user!
+  after_action :verify_authorized
 
   def index
+    authorize Course
     @courses = policy_scope(Course)
   end
 end
