ca2b44dd26b2ef0fda521d4fd9785557a4152a61
Eric-Guo <eric@managebac.com>
2018-07-29 14:09:14 +0800

    New course model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_005511) do
+ActiveRecord::Schema.define(version: 2018_07_29_053923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 2018_06_18_005511) do
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_company_users_on_company_id"
     t.index ["user_id"], name: "index_company_users_on_user_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.string "address"
+    t.integer "capacity"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.boolean "published", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ test/models/course_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class CourseTest < ActiveSupport::TestCase
+  test 'Course svsi_course valid' do
+    svsi_course = courses(:svsi_course)
+    assert svsi_course.valid?
+    assert svsi_course.published
+  end
+
+  test 'Course audio_architecture_course valid' do
+    audio_architecture_course = courses(:audio_architecture_course)
+    assert audio_architecture_course.valid?
+    assert_not audio_architecture_course.published
+  end
+end
