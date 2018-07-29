64491d96b92d01116742311dfdd98ba79cd8bf9b
Eric-Guo <eric@managebac.com>
2018-07-29 21:59:30 +0800

    Allow create courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,4 +11,21 @@ class CoursesController < ApplicationController
     @course = Course.new
     authorize @course
   end
+
+  def create
+    @course = Course.new(course_params)
+    authorize @course
+    if @course.save
+      redirect_to courses_path, notice: "Course #{@course.title} created."
+    else
+      flash[:alert] = @course.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def course_params
+    @course_params ||= params.require(:course).permit(:title, :address, :capacity, :start_time, :end_time)
+  end
 end
