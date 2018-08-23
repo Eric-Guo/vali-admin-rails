81f55d798c6a953fbf26797fe03318a223d49c9c
Eric-Guo <eric@managebac.com>
2018-08-24 01:46:04 +0800

    All page must be login

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
+    sign_in users(:shin)
     get root_url
     assert_response :success
   end
