9950d6f74b075eacc12866aa5729a2e3aedd59ac
Eric-Guo <eric@managebac.com>
2018-08-31 08:15:22 +0800

    Using DNS name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'harman_training', user: 'deployer', roles: %w{app db web}
+server 'harman.cloud-mes.com', user: 'deployer', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
