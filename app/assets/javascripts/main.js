d469cdc4cb17bee65d32a37ce408d91a74a1e386
Eric-Guo <eric@managebac.com>
2018-09-02 17:51:38 +0800

    Show list of course trainees

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -52,6 +52,7 @@ document.addEventListener("turbolinks:load", function() {
   $('#users-table').DataTable();
   $('#companies-table').DataTable();
   $('#courses-table').DataTable();
+  $('#course-trainees-table').DataTable();
   $('#circular-vertical-markets-select').select2();
   $('#course-vertical-markets-select').select2();
   $('#to-filter-company-id').select2();
