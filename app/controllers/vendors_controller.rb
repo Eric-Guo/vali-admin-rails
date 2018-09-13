161dc4ad8fc0e1fdeadc5664d958538123aaff05
Eric-Guo <eric@managebac.com>
2018-09-13 21:26:43 +0800

    Allow add company user as first level vendor

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -9,12 +9,16 @@ class VendorsController < ApplicationController
 
   def new_user
     @user = User.new
+    @new_user_random_password = SecureRandom.hex(10)
     @companies = policy_scope(Company).where(id: params[:company_ids])
   end
 
   def create_user
     company = policy_scope(Company).find user_params[:create_user_via_company_id]
-    user = User.create!(user_params)
+    user = User.new(user_params)
+    user.skip_confirmation_notification!
+    user.save
+    user.lock_access!
     CompanyUser.find_or_create_by!(company: company, user: user)
     redirect_to vendors_path, status: :found, notice: 'User created.'
   end
