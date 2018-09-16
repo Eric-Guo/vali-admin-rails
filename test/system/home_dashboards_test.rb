422734d3176a042b0d3d9fbd12f21742f6be978a
Eric-Guo <eric@managebac.com>
2018-09-16 11:22:53 +0800

    Fix test

--- test/system/home_dashboards_test.rb
+++ test/system/home_dashboards_test.rb
@@ -4,7 +4,7 @@ class HomeDashboardsTest < ApplicationSystemTestCase
   test 'visiting the home dashboard' do
     visit root_url
 
-    assert_selector 'h1', text: '通知'
-    assert_selector 'a.btn-dark', text: '登录'
+    assert_selector 'h2', text: '哈曼培训报名系统'
+    assert_selector 'button.btn-primary', text: '登录'
   end
 end
