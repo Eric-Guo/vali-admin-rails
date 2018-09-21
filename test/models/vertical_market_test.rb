dc8817e43820f861cd28e256ed814764d085a866
Eric-Guo <eric@managebac.com>
2018-09-21 21:55:23 +0800

    Combine akg and mi into akg_mi

--- test/models/vertical_market_test.rb
+++ test/models/vertical_market_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class VerticalMarketTest < ActiveSupport::TestCase
   test 'Vertical Market akg valid' do
-    akg = vertical_markets(:akg)
+    akg = vertical_markets(:akg_mi)
     assert akg.valid?
     assert_equal akg.companies.count, 4
     assert_equal akg.courses.count, 4
