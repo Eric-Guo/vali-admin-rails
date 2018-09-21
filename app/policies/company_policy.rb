908567b05420236c863cd3996fdb9aa04107d042
Eric-Guo <eric@managebac.com>
2018-09-21 22:29:57 +0800

    Only show approved company.

--- app/policies/company_policy.rb
+++ app/policies/company_policy.rb
@@ -11,7 +11,10 @@ class CompanyPolicy < ApplicationPolicy
       if user.super_admin?
         scope.all
       elsif user.admined_vm.present?
-        user.admined_vm.companies
+        user_admined_vm_company_ids = user.admined_vm.companies.pluck(:id)
+        user_managed_company_ids = user_admined_vm_company_ids +
+                                   Company.where(managed_by_company_id: user_admined_vm_company_ids).pluck(:id)
+        scope.where(id: user_managed_company_ids)
       elsif user.first_level_vendor?
         user_belongs_to_company_ids = user.companies.pluck(:id)
         user_managed_company_ids = user_belongs_to_company_ids +
