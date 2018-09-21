87f58f8f9e0afcff2a82fc9a1cd456cde90ce943
Eric-Guo <eric@managebac.com>
2018-09-21 21:55:23 +0800

    Combine akg and mi into akg_mi

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -6,7 +6,7 @@ class CompaniesControllerTest < ActionDispatch::IntegrationTest
     post create_2nd_vendor_companies_url, params: { user:
       { email: '2nd@company.com', name: '金无命', title: '剑客',
         phone: '13311825138', co_name: '金钱帮', city: '上海',
-        district: '杨浦', address: '山庄', vm_ids: [vertical_markets(:akg).id, vertical_markets(:amx).id],
+        district: '杨浦', address: '山庄', vm_ids: [vertical_markets(:akg_mi).id, vertical_markets(:amx).id],
         create_user_via_company_id: users(:eric).companies.first.id } }
     user = User.find_by! email: '2nd@company.com'
     assert_equal user.name, '金无命'
