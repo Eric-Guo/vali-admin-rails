5fdd66c4f698ccf2f18be152a02efdf0b207705d
Eric-Guo <eric@managebac.com>
2018-09-26 09:12:39 +0800

    New domain name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "harman_vendor_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://harman.cloud-mes.com' }
+  config.action_mailer.default_url_options = { host: 'https://prochina-training.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
