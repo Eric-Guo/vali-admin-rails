94e222f9a03ee1784f7abfe785776195a56c0172
Eric-Guo <eric@managebac.com>
2018-09-26 08:05:18 +0800

    New sendgrid API key.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: 'apikey',
+  password: Rails.application.credentials.sendgrid_apikey,
+  domain: 'cloud-mes.com',
+  address: 'smtp.sendgrid.net',
+  port: 465,
+  enable_starttls_auto: true
+}
