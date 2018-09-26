3405ad2eaa3b22893a2728a9b66cf089edf0395f
Eric-Guo <eric@managebac.com>
2018-09-26 09:12:39 +0800

    New domain name.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'harman.cloud-mes.com', user: 'deployer', roles: %w{app db web}
+server 'prochina-training.com', user: 'deployer', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   user_name: 'apikey',
   password: Rails.application.credentials.sendgrid_apikey,
-  domain: 'cloud-mes.com',
+  domain: 'prochina-training.com',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
