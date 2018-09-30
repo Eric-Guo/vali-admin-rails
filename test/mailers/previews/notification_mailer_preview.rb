cb4231b3c37b71445220446da47d11027797efa4
Eric-Guo <eric@managebac.com>
2018-09-30 15:03:50 +0800

    Will sent the new circular to relative people.

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -4,4 +4,9 @@ class NotificationMailerPreview < ActionMailer::Preview
     company = Company.find_by(name: '上投摩根基金管理有限公司')
     NotificationMailer.with(vertical_market_company: company.vertical_market_companies.first).new_company_email
   end
+
+  def new_circular_email
+    circular = Circular.find_by(title: '2019年度Rails技术认证培训会')
+    NotificationMailer.with(vertical_market_circular: circular.vertical_market_circulars.first).new_circular_email
+  end
 end
