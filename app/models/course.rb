fb1cd931cfd521fd78d0a0f83229ab07a10031e9
Eric-Guo <eric@managebac.com>
2018-10-16 20:01:32 +0800

    Fix course available order.

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,7 +7,7 @@ class Course < ApplicationRecord
 
   validates_presence_of :title, :address, :capacity, :start_time, :end_time
   scope :not_published, -> { where(published: false) }
-  scope :available, -> { where(published: true).where('start_time > ?', Time.zone.now) }
+  scope :available, -> { where(published: true).where('start_time > ?', Time.zone.now).order(id: :desc) }
 
   def status
     if end_time < Time.current
