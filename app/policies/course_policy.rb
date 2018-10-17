e93733b9f2eec50b39221fadb0d79be64167fc50
Eric-Guo <eric@managebac.com>
2018-10-17 23:15:38 +0800

    Only list the vm admin own VM course.

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -11,11 +11,7 @@ class CoursePolicy < ApplicationPolicy
       if user.super_admin?
         scope.all
       elsif user.admined_vm.present?
-        user_admined_vm_company_ids = user.admined_vm.companies.pluck(:id)
-        user_managed_company_ids = user_admined_vm_company_ids +
-                                   Company.where(managed_by_company_id: user_admined_vm_company_ids).pluck(:id)
-        allow_to_see_vm_ids = VerticalMarketCompany.where(company_id: user_managed_company_ids).pluck(:vertical_market_id)
-        allow_to_see_course_ids = VerticalMarketCourse.where(vertical_market_id: allow_to_see_vm_ids).pluck(:course_id)
+        allow_to_see_course_ids = VerticalMarketCourse.where(vertical_market_id: user.admined_vm.id).pluck(:course_id)
         scope.where(id: allow_to_see_course_ids)
       elsif user.first_level_vendor?
         user_belongs_to_company_ids = user.companies.pluck(:id)
