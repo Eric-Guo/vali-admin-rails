76db6b0161e56f2390873c6f80b3d7f7f33301ff
Eric-Guo <eric@managebac.com>
2018-10-26 11:37:34 +0800

    Nested remove company callback.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,8 @@
 class Company < ApplicationRecord
-  has_many :company_users
+  has_many :company_users, dependent: :destroy
   has_many :users, through: :company_users
-  has_many :vertical_market_companies
+  has_many :vertical_market_companies, dependent: :destroy
   has_many :vertical_markets, through: :vertical_market_companies
   belongs_to :managed_by_company, class_name: :Company, optional: true
-  has_many :managed_companies, class_name: :Company, foreign_key: :managed_by_company_id
+  has_many :managed_companies, class_name: :Company, foreign_key: :managed_by_company_id, dependent: :destroy
 end
